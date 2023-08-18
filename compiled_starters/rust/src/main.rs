62cf57d81449ef648e669ffcf529459e42a47ba5
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-08-18 18:29:44 +0100

    Update Rust dependencies

--- compiled_starters/rust/src/main.rs
+++ compiled_starters/rust/src/main.rs
@@ -2,7 +2,14 @@ use std::env;
 use std::io;
 use std::process;
 
-fn match_pattern(input_line: &str, pattern: &str) -> bool {
+enum Value {
+    Number(Number),
+    String(String),
+    Array(Vec<Value>),
+    Object(Map<String, Value>),
+}
+
+fn decode_bencoded_value(encoded_value: &str) -> Value {
     if pattern.chars().count() == 1 {
         return input_line.contains(pattern);
     } else {
@@ -10,20 +17,31 @@ fn match_pattern(input_line: &str, pattern: &str) -> bool {
     }
 }
 
-// Usage: echo <input_text> | your_bittorrent.sh -E <pattern>
+// Usage: your_bittorrent.sh decode "<encoded_value>"
 fn main() {
     // You can use print statements as follows for debugging, they'll be visible when running tests.
     println!("Logs from your program will appear here!");
 
-    if env::args().nth(1).unwrap() != "-E" {
-        println!("Expected first argument to be '-E'");
-        process::exit(1);
+    let args: Vec<String> = env::args().collect();
+    let command = args[1];
+
+    if command == "decode" {
+        // Decode input
+    } else 
+        println!("unknown command: {}", args[1])
     }
 
-    let pattern = env::args().nth(2).unwrap();
-    let mut input_line = String::new();
-
-    io::stdin().read_line(&mut input_line).unwrap();
+    // Uncomment this block to pass the first stage
+    // let args: Vec<String> = env::args().collect();
+    // if args[1] == "init" {
+    //     fs::create_dir(".git").unwrap();
+    //     fs::create_dir(".git/objects").unwrap();
+    //     fs::create_dir(".git/refs").unwrap();
+    //     fs::write(".git/HEAD", "ref: refs/heads/master\n").unwrap();
+    //     println!("Initialized git directory")
+    // } else {
+    //     println!("unknown command: {}", args[1])
+    // }
 
     // Uncomment this block to pass the first stage
     // if match_pattern(&input_line, &pattern) {
