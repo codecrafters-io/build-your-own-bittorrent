848c239b19f79103feef91d0f2a22db7741d620f
Anugrah Singhal <anugrahsinghal1@gmail.com>
2023-09-07 01:38:46 +0000

    working script without jbang

--- compiled_starters/java/your_bittorrent.sh
+++ compiled_starters/java/your_bittorrent.sh
@@ -19,10 +19,14 @@ tmpFile=$(mktemp -d)
 # Location of JAR used by source code
 # com.fasterxml.jackson.core:jackson-databind:2.14.2
 # jackson="/home/gitpod/.m2/repository/com/fasterxml/jackson/core"
+# databind="jackson-databind/2.14.2/jackson-databind-2.14.2.jar"
+# core="jackson-core/2.14.2/jackson-core-2.14.2.jar"
+# annotations="jackson-annotations/2.14.2/jackson-annotations-2.14.2.jar"
+
 jackson="jars"
-databind="jackson-databind/2.14.2/jackson-databind-2.14.2.jar"
-core="jackson-core/2.14.2/jackson-core-2.14.2.jar"
-annotations="jackson-annotations/2.14.2/jackson-annotations-2.14.2.jar"
+databind="jackson-databind-2.14.2.jar"
+core="jackson-core-2.14.2.jar"
+annotations="jackson-annotations-2.14.2.jar"
 
 # Source code 
 srcDir=src/main/java
@@ -32,11 +36,14 @@ outDir="$tmpFile"
 
 # Compile with classpath containing depJar
 # "${jackson}\${core}"
-echo "${jackson}/${core}" "${jackson}/${annotations}" "${jackson}/${databind}"
+# echo "${jackson}/${core}" "${jackson}/${annotations}" "${jackson}/${databind}"
 classpath="${jackson}/${core}:${jackson}/${annotations}:${jackson}/${databind}"
+# java -version
+# echo $classpath
 # javac -sourcepath $srcDir -d $outDir -cp "$classpath" $srcDir/*.java "$@"
 # javac -sourcepath $srcDir -d $outDir -cp "${jackson}/${core}" "${jackson}/${annotations}" "${jackson}/${databind}" $srcDir/*.java
-javac -cp "$classpath" -sourcepath src/main/java src/main/java/Main.java -d "$tmpFile"
+javac -classpath "$classpath" -sourcepath src/main/java src/main/java/Main.java -d "$tmpFile"
+# echo "compiled"
 jar cf java_bittorrent.jar -C "$tmpFile"/ .
 exec java -cp "$classpath:java_bittorrent.jar" Main "$@"
 
