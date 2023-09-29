2f0b3846a34ed854a7a453c3b4361ddf219fe981
libmartinito <lib.martinito@pm.me>
2023-09-29 22:55:37 +0800

    Used file timestamp modification as reference for rebuilding

--- starter_templates/java/your_bittorrent.sh
+++ starter_templates/java/your_bittorrent.sh
@@ -6,9 +6,23 @@
 #
 # DON'T EDIT THIS!
 set -e
-# Check if directory exists
-if [ ! -d "/tmp/codecrafters-bittorrent-target" ]; then
-  # Download dependencies if it does not exist
-  mvn -B --quiet package -Ddir=/tmp/codecrafters-bittorrent-target
+FILE_TO_WATCH="./src/main/java/Main.java"
+LAST_BUILD_FILE=".last_build_timestamp"
+
+if [[ "$(uname)" == "Darwin" ]]; then
+    CURRENT_TIMESTAMP=$(stat -f '%m' "$FILE_TO_WATCH")
+else
+    CURRENT_TIMESTAMP=$(stat -c '%Y' "$FILE_TO_WATCH")
+fi
+
+if [ -f "$LAST_BUILD_FILE" ]; then
+    LAST_BUILD_TIMESTAMP=$(cat "$LAST_BUILD_FILE")
+else
+    LAST_BUILD_TIMESTAMP=0
+fi
+
+if [ "$CURRENT_TIMESTAMP" -gt "$LAST_BUILD_TIMESTAMP" ]; then
+    mvn -B --quiet package -Ddir=/tmp/codecrafters-bittorrent-target
+    echo "$CURRENT_TIMESTAMP" > "$LAST_BUILD_FILE"
 fi
 exec java -jar /tmp/codecrafters-bittorrent-target/java_bittorrent.jar "$@"
