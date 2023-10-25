c32a127741d806e603c05f7c77391ce03bcf8304
Paul Kuruvilla <rohitpaulk@gmail.com>
2023-10-25 15:22:11 +0100

    consistent spacing

--- starter_templates/elixir/lib/bittorrent.ex
+++ starter_templates/elixir/lib/bittorrent.ex
@@ -4,8 +4,9 @@ defmodule Bittorrent.CLI do
           ["decode" | [encoded_str | _]] ->
               # You can use print statements as follows for debugging, they'll be visible when running tests.
               IO.puts("Logs from your program will appear here!")
+
               # Uncomment this block to pass the first stage
-              #Bencode.decode(encoded_str)
+              # Bencode.decode(encoded_str)
           [command | _] ->
               IO.puts("Unknown command: #{command}")
               System.halt(1)
@@ -32,5 +33,3 @@ defmodule Bencode do
 
     def decode(_), do: "Invalid encoded value: not binary"
 end
-
-
