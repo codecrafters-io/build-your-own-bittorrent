c8db49aa1337b592e29072fda6b08512bbd664ce
urielsalis <uriel@urielsalis.me>
2023-10-25 13:09:06 +0200

    Add kotlin support
    Adds support for Kotlin 1.9 using a similar starter to the Java one.

---
+++ solutions/kotlin/01-bencode-string/code/src/main/kotlin/Main.kt
@@ -0,0 +1,32 @@
+import com.google.gson.Gson;
+// import com.dampcake.bencode.Bencode; - available if you need it!
+
+val gson = Gson()
+
+fun main(args: Array<String>) {
+    val command = args[0]
+    when (command) {
+        "decode" -> {
+            val bencodedValue = args[1]
+            try {
+                val decoded = decodeBencode(bencodedValue)
+                println(gson.toJson(decoded))
+            } catch (e: RuntimeException) {
+                println(e.message)
+            }
+            return
+        }
+        else -> println("Unknown command $command")
+    }
+}
+
+fun decodeBencode(bencodedString: String): String {
+    when {
+        Character.isDigit(bencodedString[0]) -> {
+            val firstColonIndex = bencodedString.indexOfFirst { it == ':' }
+            val length = Integer.parseInt(bencodedString.substring(0, firstColonIndex))
+            return bencodedString.substring(firstColonIndex + 1, firstColonIndex + 1 + length)
+        }
+        else -> TODO("Only strings are supported at the moment")
+    }
+}
