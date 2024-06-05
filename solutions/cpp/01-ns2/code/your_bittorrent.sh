16db9d2984688868d873e4a76ec441b4a283c5df
Ryan Gang <ryan-gg@outlook.com>
2024-06-05 14:13:35 +0530

    feat: update spawn scripts

--- solutions/cpp/01-ns2/code/your_bittorrent.sh
+++ solutions/cpp/01-ns2/code/your_bittorrent.sh
@@ -6,6 +6,7 @@
 #
 # DON'T EDIT THIS!
 set -e
-cmake $(dirname $0) -B build > /dev/null
-cmake --build build > /dev/null
+# vcpkg & cmake are required. 
+cmake -B build -S . -DCMAKE_TOOLCHAIN_FILE=${VCPKG_ROOT}/scripts/buildsystems/vcpkg.cmake
+cmake --build ./build
 exec ./build/bittorrent "$@"
