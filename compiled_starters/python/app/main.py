9b03c645b9f28bc8bcbbe9fb84bebc8cffcfe60a
Andy Li <1450947+andy1li@users.noreply.github.com>
2025-10-24 00:03:51 +0900

    CC-1984: Change uncomment code text

--- compiled_starters/python/app/main.py
+++ compiled_starters/python/app/main.py
@@ -37,7 +37,7 @@ def main():
 
             raise TypeError(f"Type not serializable: {type(data)}")
 
-        # Uncomment this block to pass the first stage
+        # Uncomment the code below to pass the first stage
         # print(json.dumps(decode_bencode(bencoded_value), default=bytes_to_str))
     else:
         raise NotImplementedError(f"Unknown command {command}")
--- starter_templates/java/code/src/main/java/Main.java
+++ starter_templates/java/code/src/main/java/Main.java
@@ -10,7 +10,7 @@ public class Main {
     
     String command = args[0];
     if("decode".equals(command)) {
-      //  Uncomment this block to pass the first stage
+      //  Uncomment the code below to pass the first stage
       //  String bencodedValue = args[1];
       //  String decoded;
       //  try {
